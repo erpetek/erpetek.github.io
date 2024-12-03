59d6ddd755e95b2f39512f5e8df4d95ffe7cab10
erpetek <piotrek.erpetek@gmail.com>
2024-12-03 23:50:46 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,526 @@
+const kapitelEins = [
+    {"ger": "die Absicht", "pol": "zamiar"},
+    {"ger": "der Englischkurs", "pol": "kurs języka angielskiego"},
+    {"ger": "der Führerschein", "pol": "prawo jazdy"},
+    {"ger": "die Lust", "pol": "ochota"},
+    {"ger": "die Nachhilfe", "pol": "korepetycje"},
+    {"ger": "der Plan", "pol": "plan"},
+    {"ger": "das Schuljahr", "pol": "rok szkolny"},
+    {"ger": "sich anmelden", "pol": "zgłosić się"},
+    {"ger": "schaffen", "pol": "radzić sobie"},
+    {"ger": "schauen", "pol": "oglądać"},
+    {"ger": "sich vorbereiten", "pol": "przygotowywać (się)"},
+    {"ger": "selbst bezahlen", "pol": "płacić samemu"},
+    {"ger": "Filme auf Englisch", "pol": "filmy po angielsku"},
+    {"ger": "Geld verdienen", "pol": "zarabiać pieniądze"},
+    {"ger": "Probleme in Mathe", "pol": "problemy z matematyką"},
+    {"ger": "das Abitur", "pol": "matura"},
+    {"ger": "die Angst", "pol": "strach"},
+    {"ger": "die Inspiration", "pol": "inspiracja"},
+    {"ger": "die Kraft", "pol": "siła"},
+    {"ger": "die Präsentation", "pol": "prezentacja"},
+    {"ger": "das Thema", "pol": "temat"},
+    {"ger": "der Unterricht", "pol": "lekcja, zajęcia"},
+    {"ger": "endlich", "pol": "nareszcie"},
+    {"ger": "intensiv", "pol": "intensywny"},
+    {"ger": "logisch", "pol": "logiczny"},
+    {"ger": "motiviert", "pol": "zmotywowany"},
+    {"ger": "ständig", "pol": "ciągle"},
+    {"ger": "stark", "pol": "silny"},
+    {"ger": "ziemlich", "pol": "dosyć"},
+    {"ger": "aufpassen", "pol": "uważać"},
+    {"ger": "befürchten", "pol": "obawiać się"},
+    {"ger": "einschlafen", "pol": "zasypiać"},
+    {"ger": "sich erholen", "pol": "odpoczywać"},
+    {"ger": "sich konzentrieren", "pol": "koncentrować się"},
+    {"ger": "schaffen", "pol": "radzić sobie"},
+    {"ger": "verlieren", "pol": "stracić"},
+    {"ger": "nichts machen", "pol": "nic nie robić"},
+    {"ger": "im Stress sein", "pol": "być w stresie"},
+    {"ger": "Informationen suchen", "pol": "szukać informacji"},
+    {"ger": "Das ist nicht lange hin!", "pol": "To nie tak długo."},
+    {"ger": "das Angebot", "pol": "oferta"},
+    {"ger": "die Anzeige", "pol": "ogłoszenie"},
+    {"ger": "der Fortschritt", "pol": "postęp"},
+    {"ger": "der Intensivkurs", "pol": "kurs intensywny"},
+    {"ger": "der Kursbeginn", "pol": "początek kursu"},
+    {"ger": "der Kursdauer", "pol": "czas trwania kursu"},
+    {"ger": "der Kursumfang", "pol": "tu: liczba godzin kursu"},
+    {"ger": "das Lehrwerk", "pol": "podręcznik"},
+    {"ger": "der Muttersprachler", "pol": "native speaker"},
+    {"ger": "das Niveau", "pol": "poziom"},
+    {"ger": "der Preis", "pol": "cena"},
+    {"ger": "der Sommerkurs", "pol": "kurs letni"},
+    {"ger": "die Sprachreise", "pol": "wyjazd połączony z nauką języka"},
+    {"ger": "die Unterrichtseinheit", "pol": "jednostka lekcyjna"},
+    {"ger": "das Unterrichtsmaterial", "pol": "materiał dydaktyczny"},
+    {"ger": "das Zertifikat", "pol": "certyfikat"},
+    {"ger": "einfach", "pol": "przeżywać"},
+    {"ger": "fehlen", "pol": "brakować"},
+    {"ger": "reisen", "pol": "podróżować"},
+    {"ger": "ein paar Infos", "pol": "kilka informacji"},
+    {"ger": "im Voraus", "pol": "z góry"},
+    {"ger": "in kurzer Zeit", "pol": "w krótkim czasie"},
+    {"ger": "das Archiv", "pol": "archiwum"},
+    {"ger": "der Bürger", "pol": "mieszczanin"},
+    {"ger": "das Gründungsjahr", "pol": "rok założenia"},
+    {"ger": "das Jubiläum", "pol": "jubileusz"},
+    {"ger": "die Klosterschule", "pol": "szkoła przyklasztorna"},
+    {"ger": "der Mönch", "pol": "mnich"},
+    {"ger": "die Naturwissenschaften", "pol": "nauki przyrodnicze"},
+    {"ger": "die Regel", "pol": "reguła"},
+    {"ger": "das Schulhaus", "pol": "budynek szkolny"},
+    {"ger": "die Strafe", "pol": "kara"},
+    {"ger": "bezahlen", "pol": "opłacać"},
+    {"ger": "verraten", "pol": "zdradzać"},
+    {"ger": "gemeinsam", "pol": "razem, wspólnie"},
+    {"ger": "getrennt", "pol": "oddzielnie"},
+    {"ger": "heutzutage", "pol": "obecnie"},
+    {"ger": "kostenlos", "pol": "bezpłatny"},
+    {"ger": "öffentlich", "pol": "publiczny"},
+    {"ger": "reich", "pol": "bogaty"},
+    {"ger": "streng", "pol": "surowy, wymagający"},
+    {"ger": "ein Jubiläum feiern", "pol": "obchodzić jubileusz"}
+];
+const kapitelZwei = [
+    {
+        "ger": "das Bargeld",
+        "pol": "gotówka"
+    },
+    {
+        "ger": "das Kleingeld",
+        "pol": "drobne pieniądze"
+    },
+    {
+        "ger": "das Rückgeld",
+        "pol": "reszta (pieniądze)"
+    },
+    {
+        "ger": "bar",
+        "pol": "w gotówce, gotówkowo"
+    },
+    {
+        "ger": "brauchen",
+        "pol": "potrzebować"
+    },
+    {
+        "ger": "dabei haben",
+        "pol": "mieć przy sobie"
+    },
+    {
+        "ger": "zahlen",
+        "pol": "płacić"
+    },
+    {
+        "ger": "bar zahlen",
+        "pol": "płacić gotówką"
+    },
+    {
+        "ger": "Ich habe es passend.",
+        "pol": "Mam odliczoną kwotę."
+    },
+    {
+        "ger": "die Bestätigung",
+        "pol": "potwierdzenie"
+    },
+    {
+        "ger": "die Karte",
+        "pol": "karta"
+    },
+    {
+        "ger": "das Lesegerät",
+        "pol": "czytnik"
+    },
+    {
+        "ger": "die PIN-Nummer",
+        "pol": "numer PIN"
+    },
+    {
+        "ger": "die Taste",
+        "pol": "przycisk"
+    },
+    {
+        "ger": "kontaktlos",
+        "pol": "bezdotykowo"
+    },
+    {
+        "ger": "drücken",
+        "pol": "naciskać"
+    },
+    {
+        "ger": "ein geben",
+        "pol": "wprowadzać"
+    },
+    {
+        "ger": "Das macht...",
+        "pol": "To wynosi..."
+    },
+    {
+        "ger": "das Aufladen",
+        "pol": "doładowanie"
+    },
+    {
+        "ger": "ein zahlen",
+        "pol": "wpłacać"
+    },
+    {
+        "ger": "eröffnen",
+        "pol": "otwierać (konto)"
+    },
+    {
+        "ger": "unterschreiben",
+        "pol": "podpisywać"
+    },
+    {
+        "ger": "vorbei kommen",
+        "pol": "wpadać"
+    },
+    {
+        "ger": "eine Bankkarte bekommen",
+        "pol": "dostać kartę płatniczą"
+    },
+    {
+        "ger": "ein Dokument ausfüllen",
+        "pol": "wypełniać dokument"
+    },
+    {
+        "ger": "ein Konto eröffnen",
+        "pol": "otwierać konto"
+    },
+    {
+        "ger": "per Post schicken",
+        "pol": "wysyłać pocztą"
+    },
+    {
+        "ger": "der Auftrag",
+        "pol": "zlecenie"
+    },
+    {
+        "ger": "der Betrag",
+        "pol": "kwota"
+    },
+    {
+        "ger": "die Funktion",
+        "pol": "funkcja"
+    },
+    {
+        "ger": "die Geheimzahl",
+        "pol": "numer PIN"
+    },
+    {
+        "ger": "der Kontostand",
+        "pol": "stan konta"
+    },
+    {
+        "ger": "die Kreissparkasse",
+        "pol": "kasa oszczędności"
+    },
+    {
+        "ger": "die Taste",
+        "pol": "klawisz"
+    },
+    {
+        "ger": "aus drücken",
+        "pol": "wyciskać"
+    },
+    {
+        "ger": "aus wählen",
+        "pol": "wybierać"
+    },
+    {
+        "ger": "ein geben",
+        "pol": "podawać (numer)"
+    },
+    {
+        "ger": "die PIN eingeben",
+        "pol": "podawać PIN"
+    },
+    {
+        "ger": "mit der grünen Taste",
+        "pol": "zielonym klawiszem"
+    },
+    {
+        "ger": "das Aufladen",
+        "pol": "doładowanie"
+    },
+    {
+        "ger": "die Bankkarte",
+        "pol": "karta płatnicza"
+    },
+    {
+        "ger": "das Dokument",
+        "pol": "dokument"
+    },
+    {
+        "ger": "der Geldautomat",
+        "pol": "bankomat"
+    },
+    {
+        "ger": "das Girokonto",
+        "pol": "konto oszczędnościowo-rozliczeniowe"
+    },
+    {
+        "ger": "die Kondition",
+        "pol": "warunek"
+    },
+    {
+        "ger": "die Kontoführung",
+        "pol": "prowadzenie konta"
+    },
+    {
+        "ger": "das Online-Banking",
+        "pol": "bankowość internetowa"
+    },
+    {
+        "ger": "der Personalausweis",
+        "pol": "dowód osobisty"
+    },
+    {
+        "ger": "das Sparkonto",
+        "pol": "konto oszczędnościowe"
+    },
+    {
+        "ger": "die Website",
+        "pol": "strona internetowa"
+    },
+    {
+        "ger": "der Zugang",
+        "pol": "dostęp"
+    },
+    {
+        "ger": "bargeldlos",
+        "pol": "bezgotówkowy"
+    },
+    {
+        "ger": "kostenlos",
+        "pol": "bezpłatny"
+    },
+    {
+        "ger": "ab heben",
+        "pol": "podejmować (pieniądze)"
+    },
+    {
+        "ger": "an bieten",
+        "pol": "oferować"
+    },
+    {
+        "ger": "das Ergebnis",
+        "pol": "rezultat"
+    },
+    {
+        "ger": "die Rate",
+        "pol": "rata"
+    },
+    {
+        "ger": "die Schulden",
+        "pol": "długi"
+    },
+    {
+        "ger": "die Spardose",
+        "pol": "skarbonka"
+    },
+    {
+        "ger": "das Sparschwein",
+        "pol": "świnka skarbonka"
+    },
+    {
+        "ger": "das Taschengeld",
+        "pol": "kieszonkowe"
+    },
+    {
+        "ger": "aus geben",
+        "pol": "wydawać"
+    },
+    {
+        "ger": "sich lohnen",
+        "pol": "opłacać się"
+    },
+    {
+        "ger": "sparen",
+        "pol": "oszczędzać"
+    },
+    {
+        "ger": "um gehen",
+        "pol": "obchodzić się"
+    },
+    {
+        "ger": "alle",
+        "pol": "wszyscy, wszystkie"
+    },
+    {
+        "ger": "einige",
+        "pol": "kilku, kilkoro"
+    },
+    {
+        "ger": "jeder",
+        "pol": "każdy"
+    },
+    {
+        "ger": "keiner",
+        "pol": "nikt"
+    },
+    {
+        "ger": "viel",
+        "pol": "wiele"
+    },
+    {
+        "ger": "wenig",
+        "pol": "mało"
+    },
+    {
+        "ger": "mit dem Geld umgehen",
+        "pol": "obchodzić się z pieniędzmi"
+    }
+]
+const prateritum = [
+    {"ger": "lernen", "pra": "lernte", "pol": "uczyć się"},
+    {"ger": "sein", "pra": "war", "pol": "być"},
+    {"ger": "besuchen", "pra": "besuchte", "pol": "uczęszczać"},
+    {"ger": "unterrichten", "pra": "unterrichtete", "pol": "nauczać"},
+    {"ger": "stehen", "pra": "stand", "pol": "stać"},
+    {"ger": "geben", "pra": "gab", "pol": "dawać"},
+    {"ger": "gehen", "pra": "ging", "pol": "iść, chodzić"},
+    {"ger": "müssen", "pra": "musste", "pol": "musieć"},
+    {"ger": "dürfen", "pra": "durfte", "pol": "mieć pozwolenie"},
+    {"ger": "arbeiten", "pra": "arbeitete", "pol": "pracować"},
+    {"ger": "bekommen", "pra": "bekam", "pol": "dostać, otrzymać"}
+]
+let preferences = [false, false, false];
+let availableWords = create_word_array(true, true, true);
+let answer;
+
+const appDiv = document.getElementById("app");
+const resultDiv = document.getElementById("result");
+
+const lastInputSpan = document.getElementById("last-input");
+const expectedSpan = document.getElementById("output");
+const toTranslateSpan = document.getElementById("to-translate");
+
+const input = document.getElementById("input");
+const submitBtn = document.getElementById("submit");
+const okBtn = document.getElementById("ok");
+
+const opcje = document.getElementById("opcje");
+const optionsok = document.getElementById("optionsok");
+const optionsDiv = document.getElementById("options");
+
+let mode = 0;
+
+function create_word_array() {
+    let array = [];
+    if (preferences[0]) array = array.concat(kapitelEins);
+    if (preferences[1]) array = array.concat(kapitelZwei);
+    if (preferences[2]) array = array.concat(prateritum);
+    return array;
+}
+
+function apply_settings() {
+    let ok = document.querySelectorAll(".option input:checked");
+    preferences[0] = false;
+    preferences[1] = false;
+    preferences[2] = false;
+    ok.forEach(e => {
+        switch (e.id) {
+            case "gerpol":
+                mode = 0;
+                break;
+            case "polger":
+                mode = 1;
+                break;
+            case "random":
+                mode = 2;
+                break;
+            case "keins":
+                preferences[0] = true;
+                break;
+            case "kzwei":
+                preferences[1] = true;
+                break;
+            case "prateritum":
+                preferences[2] = true;
+                break;
+            default:
+                break;
+        }
+    });
+    availableWords = create_word_array();
+}
+
+{
+    let opts = JSON.parse(localStorage.getItem("options"));
+    if (opts !== undefined) {
+        for (let i = 0; i < opts.length; i++) {
+            let e = document.getElementById(opts[i][0]);
+            e.checked = opts[i][1];
+        }
+    }
+    apply_settings();
+}
+
+function rng(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function draw_word() {
+    let randomIndex = rng(0, availableWords.length - 1);
+    let word = availableWords[randomIndex];
+    availableWords.splice(randomIndex, 1);
+    let t = word.ger;
+    let a = word.pol;
+    if (word.pra !== undefined) {
+        t = `${t} => ${word.pra}`;
+    }
+    let ok = mode;
+    if (ok === 2) {
+        ok = rng(0, 1);
+    }
+    if (ok) {
+        return [t, a];
+    } else {
+        return [a, t];
+    }
+}
+
+submitBtn.addEventListener("click", function() {
+    if (answer === undefined) {
+        answer = draw_word();
+        toTranslateSpan.textContent = answer[0];
+        return;
+    }
+    lastInputSpan.textContent = input.value;
+    expectedSpan.textContent = answer[1];
+    appDiv.classList.add("hidden");
+    resultDiv.classList.remove("hidden");
+});
+
+okBtn.addEventListener("click", function() {
+    answer = draw_word();
+    toTranslateSpan.textContent = answer[0];
+    if (availableWords.length === 0)
+        availableWords = create_word_array(preferences[0], preferences[1], preferences[2]);
+    input.value = "";
+    appDiv.classList.remove("hidden");
+    resultDiv.classList.add("hidden");
+});
+
+opcje.addEventListener("click", function() {
+     optionsDiv.classList.remove("hidden");
+});
+
+optionsok.addEventListener("click", function() {
+    optionsDiv.classList.add("hidden");
+    let real = document.querySelectorAll(".option input");
+    let s = [];
+    real.forEach(e => {
+         s.push([e.id, e.checked]);
+    });
+    localStorage.setItem("options", JSON.stringify(s));
+    apply_settings();
+    answer = undefined;
+    toTranslateSpan.textContent = "Kliknij przycisk aby wylosowac ok?";
+});
+
+const tenadole = document.getElementById("te-na-dole");
+document.querySelector("#tenadole").addEventListener("click", () => {
+    tenadole.classList.toggle("hidden");
+});
